99f9684df368c5bc3e952f0ebb6e2839f75f8dde
Alejandro <alesf22@gmail.com>
2023-09-23 13:50:50 -0600

    feat: get user data from api

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Get(':username')
+  async getUser(@Param() params) {
+    const res = await this.userService.getUser(params);
+    return {
+      name: res.data.name,
+      website: res.data.blog,
+      avatar: res.data.avatar_url,
+      user_profile: res.data.url,
+    };
+  }
+}
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Octokit } from 'octokit';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private octokit;
+  constructor() {
+    this.octokit = new Octokit({
+      auth: process.env.GITHUB_ACCESS_TOKEN,
+    });
+  }
+
+  async getUser(params) {
+    return await this.octokit.request('GET /users/{username}', {
+      username: params.username,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    });
+  }
+}
